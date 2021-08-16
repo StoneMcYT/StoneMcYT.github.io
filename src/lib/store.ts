dcca2a8942249b0dcc066b74430f41b4f5bb014d
StoneMc <Stone.mc.channel@gmail.com>
2021-08-16 17:28:40 +0800

    Testing failed sadly

--- src/lib/store.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from 'svelte/store';
-import { browser } from '$app/env';
-
-export const count = writable(+((browser && localStorage.getItem('count')) || '0'));
-
-count.subscribe((val) => {
-  if (browser) return (localStorage.count = `${val}`);
-});
